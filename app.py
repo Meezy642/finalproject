a3cdf51a70c236416152446a733fdc254000edda
Meezy642 <meezyvaisex@gmail.com>
2026-06-06 18:00:28 +0700

    Configure SMTP, disable Telegram reset notification, and expose server on 0.0.0.0

--- app.py
+++ app.py
@@ -656,4 +656,4 @@ if __name__ == '__main__':
         polling_thread.start()
         
     # Running on local port 5000
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
